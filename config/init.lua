53947443bfa995eea3d08689ba11b4c57ae5a3dd
Brandon Piesch <klapptnot@gmail.com>
2024-09-27 13:00:53 -0400

    explicit data types

--- config/init.lua
+++ config/init.lua
@@ -1,9 +1,16 @@
 -- && Config loader for Spruce
 
+---@alias SpruceConfig {globals:fun():NvimGlobalsConfig; mapping:fun():NvimMappingConfig; options:fun():NvimOptionsConfig; plugins:fun():NvimPluginsConfig}
+
+---@type SpruceConfig
 local main = {
+  ---@return NvimGlobalsConfig
   globals = function() return require("config.globals"):new() end,
+  ---@return NvimMappingConfig
   mapping = function() return require("config.mapping"):new() end,
+  ---@return NvimOptionsConfig
   options = function() return require("config.options"):new() end,
+  ---@return NvimPluginsConfig
   plugins = function() return require("config.plugins"):new() end,
 } -- Return all configs classes
 
